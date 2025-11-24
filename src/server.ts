eeb6dd4057bca34f0df5fc69cbd11a8344531169
islamemon59 <islamemon59@gmail.com>
2025-11-24 23:52:58 +0600

    node server create with ts

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log("Server is running...");
+    if (req.url == "/" && req.method == "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Hello from node js with typescript",
+          path: req.url,
+        })
+      );
+    }
+  }
+);
+
+server.listen(5000, () => {
+  console.log(`Server is running on port ${5000}`);
+});
