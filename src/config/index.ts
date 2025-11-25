b8715bde39197839abed3185c4b805d97bd61a6b
islamemon59 <islamemon59@gmail.com>
2025-11-25 07:22:02 +0600

    .env config file added

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,10 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({path: path.join(process.cwd(), ".env")})
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const config = {
+  port: process.env.PORT,
+};
+
+export default config;
