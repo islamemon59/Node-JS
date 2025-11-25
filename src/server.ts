7078a36dc749e670ae54f3f1a92aaa448a9aae35
islamemon59 <islamemon59@gmail.com>
2025-11-25 08:30:09 +0600

    post request added by node js also ts node dev installed

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import config from "./config";
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("Server is running...");
+    //root request
     if (req.url == "/" && req.method == "GET") {
       res.writeHead(200, { "content-type": "application/json" });
       res.end(
@@ -12,6 +13,44 @@ const server: Server = http.createServer(
           path: req.url,
         })
       );
+    }
+
+    //check health request
+    if (req.url == "/api" && req.method == "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Health is good...",
+          path: req.url,
+        })
+      );
+    }
+
+    //post request
+    if (req.url == "/api/users" && req.method == "POST") {
+      // const user = {
+      //   id: 1,
+      //   name: "Bob",
+      // };
+      // res.writeHead(200, { "content-type": "application/json" });
+      // res.end(JSON.stringify(user));
+
+      let body = "";
+
+      req.on("data", (chunk) => {
+        body = body + chunk.toString();
+      });
+
+      req.on("end", () => {
+        try {
+          const parseBody = JSON.parse(body);
+          console.log(parseBody);
+          console.log("Check the to get current changing data");
+          res.end(JSON.stringify(parseBody));
+        } catch (error: any) {
+          console.log(error?.message);
+        }
+      });
     }
   }
 );
